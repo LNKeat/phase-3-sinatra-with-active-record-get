e6b1dfe76ffbd0b98db9b8e4c120e9e99f830186
Laura <LNKEAT@gmail.com>
2023-02-06 19:33:11 -0700

    good info for routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
 
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    games = Game.all
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(only: [:title, :price], include: {reviews: {only:[:score, :user_id]}})
   end
 
 end
